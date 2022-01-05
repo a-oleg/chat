782d6ac3760a93a3721a439e36bb014cf3380be1
a-oleg <96666697+a-oleg@users.noreply.github.com>
2022-01-05 23:39:04 +0300

    Update PostMan.java

--- ServerChat/src/oleg/PostMan.java
+++ ServerChat/src/oleg/PostMan.java
@@ -2,6 +2,7 @@ package oleg;
 
 public class PostMan implements Runnable {
     Thread postThread = new Thread(this, "Поток для почтальона");
+    //не понятно, почему не работает метод старт
     postThread.start();
 
     @Override
